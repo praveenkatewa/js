d33af0ef04a69cb15c42e09eabdb9d1accd57a4b
praveen katewa <praveenkatewa.45@gmail.com>
2024-07-18 12:09:24 +0530

    new file

--- datatype_2/data.js
+++ datatype_2/data.js
@@ -1,3 +1,57 @@
+/*
+let a=10.89;
+console.log(a)
+console.log(typeof a)
+*/
+/*
+let b="anuj";
+console.log(typeof b)
+*/
+
+/*
+let c= true;
+console.log(typeof c)
+*/
+
+
+/*
+let c;
+console.log(typeof c)
+*/
+
+/*
+let d=null;
+console.log(typeof d)
+*/
+
+
+/*
+let e= Symbol(5);
+console.log(typeof e)
+*/
+
+let e= Symbol(5);
+let f= Symbol(5);
+console.log(e==f);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // dynamic typing
 
---
+++ variable_1/page.js
@@ -0,0 +1,28 @@
+var a = "john doe";
+var somebool= true;
+if(somebool){
+     var a ="danial joan";   
+}
+console.log(a);
+
+
+
+
+
+/*
+const accountId=122;
+let accountEmail="praveen@gmail.com";
+var accountPassword="1234"
+accountCity="jaipur"
+let accountState;
+accountId=3
+accountEmail="anuj@gmail.com";
+accountPassword="132234"
+accountCity="ajmer"
+
+
+console.log(accountId);
+ console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+let fullName="tony stark";
+*/
