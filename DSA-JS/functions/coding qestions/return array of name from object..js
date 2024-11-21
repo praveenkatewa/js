6392ead019da5cb9d25ed3728e58d8c170f2a551
praveen katewa <praveenkatewa.45@gmail.com>
2024-11-21 23:46:54 +0530

    new files

---
+++ DSA-JS/functions/coding qestions/return array of name from object..js
@@ -0,0 +1,19 @@
+// create an array of names of senoir employees
+
+
+const employees=[
+  {
+    name: "prakash",
+    numofyear:5
+  },
+
+  {
+    name:"ashish",
+    numofyears:3
+  },
+  {
+    name:"riya",
+    numofyear:1
+  },
+  
+]
